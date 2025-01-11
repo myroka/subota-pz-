92f6b8e323a750ff7dece9a5b7270bf3d309bf76
Miroslav Gunko <miroslavgunko328@gmail.com>
2025-01-11 17:13:29 +0200

    Добавьте файлы проекта.

---
+++ subota(pz)/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace subota_pz_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioproverka1 = new System.Windows.Forms.RadioButton();
+            this.vopros1 = new System.Windows.Forms.Label();
+            this.radioproverka2 = new System.Windows.Forms.RadioButton();
+            this.radioproverka3 = new System.Windows.Forms.RadioButton();
+            this.rB_1_1 = new System.Windows.Forms.RadioButton();
+            this.rB_2_2 = new System.Windows.Forms.RadioButton();
+            this.rB_3_3 = new System.Windows.Forms.RadioButton();
+            this.uveren1 = new System.Windows.Forms.RadioButton();
+            this.uveren2 = new System.Windows.Forms.RadioButton();
+            this.uveren3 = new System.Windows.Forms.RadioButton();
+            this.vopros2 = new System.Windows.Forms.Label();
+            this.vopros3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioproverka1
+            // 
+            this.radioproverka1.AutoSize = true;
+            this.radioproverka1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.radioproverka1.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioproverka1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.radioproverka1.Location = new System.Drawing.Point(12, 0);
+            this.radioproverka1.Name = "radioproverka1";
+            this.radioproverka1.Size = new System.Drawing.Size(173, 35);
+            this.radioproverka1.TabIndex = 0;
+            this.radioproverka1.TabStop = true;
+            this.radioproverka1.Text = "загоняются";
+            this.radioproverka1.UseVisualStyleBackColor = false;
+            this.radioproverka1.CheckedChanged += new System.EventHandler(this.radioproverka1_CheckedChanged);
+            // 
+            // vopros1
+            // 
+            this.vopros1.AutoSize = true;
+            this.vopros1.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.vopros1.Location = new System.Drawing.Point(12, 25);
+            this.vopros1.Name = "vopros1";
+            this.vopros1.Size = new System.Drawing.Size(331, 31);
+            this.vopros1.TabIndex = 1;
+            this.vopros1.Text = "1) Гонщики гоняют да не..";
+            // 
+            // radioproverka2
+            // 
+            this.radioproverka2.AutoSize = true;
+            this.radioproverka2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.radioproverka2.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioproverka2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.radioproverka2.Location = new System.Drawing.Point(6, 59);
+            this.radioproverka2.Name = "radioproverka2";
+            this.radioproverka2.Size = new System.Drawing.Size(182, 35);
+            this.radioproverka2.TabIndex = 0;
+            this.radioproverka2.TabStop = true;
+            this.radioproverka2.Text = "не теряются";
+            this.radioproverka2.UseVisualStyleBackColor = false;
+            // 
+            // radioproverka3
+            // 
+            this.radioproverka3.AutoSize = true;
+            this.radioproverka3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.radioproverka3.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioproverka3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.radioproverka3.Location = new System.Drawing.Point(6, 121);
+            this.radioproverka3.Name = "radioproverka3";
+            this.radioproverka3.Size = new System.Drawing.Size(179, 35);
+            this.radioproverka3.TabIndex = 0;
+            this.radioproverka3.TabStop = true;
+            this.radioproverka3.Text = "обьедаются";
+            this.radioproverka3.UseVisualStyleBackColor = false;
+            // 
+            // rB_1_1
+            // 
+            this.rB_1_1.AutoSize = true;
+            this.rB_1_1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.rB_1_1.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rB_1_1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.rB_1_1.Location = new System.Drawing.Point(334, 145);
+            this.rB_1_1.Name = "rB_1_1";
+            this.rB_1_1.Size = new System.Drawing.Size(53, 35);
+            this.rB_1_1.TabIndex = 0;
+            this.rB_1_1.TabStop = true;
+            this.rB_1_1.Text = "Л";
+            this.rB_1_1.UseVisualStyleBackColor = false;
+            // 
+            // rB_2_2
+            // 
+            this.rB_2_2.AutoSize = true;
+            this.rB_2_2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.rB_2_2.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rB_2_2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.rB_2_2.Location = new System.Drawing.Point(334, 204);
+            this.rB_2_2.Name = "rB_2_2";
+            this.rB_2_2.Size = new System.Drawing.Size(125, 35);
+            this.rB_2_2.TabIndex = 0;
+            this.rB_2_2.TabStop = true;
+            this.rB_2_2.Text = "Андрей";
+            this.rB_2_2.UseVisualStyleBackColor = false;
+            // 
+            // rB_3_3
+            // 
+            this.rB_3_3.AutoSize = true;
+            this.rB_3_3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.rB_3_3.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rB_3_3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.rB_3_3.Location = new System.Drawing.Point(334, 268);
+            this.rB_3_3.Name = "rB_3_3";
+            this.rB_3_3.Size = new System.Drawing.Size(150, 35);
+            this.rB_3_3.TabIndex = 0;
+            this.rB_3_3.TabStop = true;
+            this.rB_3_3.Text = "допустим";
+            this.rB_3_3.UseVisualStyleBackColor = false;
+            // 
+            // uveren1
+            // 
+            this.uveren1.AutoSize = true;
+            this.uveren1.BackColor = System.Drawing.Color.DarkSalmon;
+            this.uveren1.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.uveren1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.uveren1.Location = new System.Drawing.Point(6, 21);
+            this.uveren1.Name = "uveren1";
+            this.uveren1.Size = new System.Drawing.Size(158, 35);
+            this.uveren1.TabIndex = 0;
+            this.uveren1.TabStop = true;
+            this.uveren1.Text = "потужнісь";
+            this.uveren1.UseVisualStyleBackColor = false;
+            // 
+            // uveren2
+            // 
+            this.uveren2.AutoSize = true;
+            this.uveren2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.uveren2.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.uveren2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.uveren2.Location = new System.Drawing.Point(6, 74);
+            this.uveren2.Name = "uveren2";
+            this.uveren2.Size = new System.Drawing.Size(195, 35);
+            this.uveren2.TabIndex = 0;
+            this.uveren2.TabStop = true;
+            this.uveren2.Text = "доллар по 45";
+            this.uveren2.UseVisualStyleBackColor = false;
+            // 
+            // uveren3
+            // 
+            this.uveren3.AutoSize = true;
+            this.uveren3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.uveren3.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.uveren3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.uveren3.Location = new System.Drawing.Point(6, 132);
+            this.uveren3.Name = "uveren3";
+            this.uveren3.Size = new System.Drawing.Size(122, 35);
+            this.uveren3.TabIndex = 0;
+            this.uveren3.TabStop = true;
+            this.uveren3.Text = "по 42??";
+            this.uveren3.UseVisualStyleBackColor = false;
+            // 
+            // vopros2
+            // 
+            this.vopros2.AutoSize = true;
+            this.vopros2.BackColor = System.Drawing.Color.RosyBrown;
+            this.vopros2.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.vopros2.Location = new System.Drawing.Point(343, 102);
+            this.vopros2.Name = "vopros2";
+            this.vopros2.Size = new System.Drawing.Size(141, 31);
+            this.vopros2.TabIndex = 1;
+            this.vopros2.Text = "2) АНдрА..";
+            // 
+            // vopros3
+            // 
+            this.vopros3.AutoSize = true;
+            this.vopros3.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.vopros3.Location = new System.Drawing.Point(587, 75);
+            this.vopros3.Name = "vopros3";
+            this.vopros3.Size = new System.Drawing.Size(115, 31);
+            this.vopros3.TabIndex = 1;
+            this.vopros3.Text = "1) хто я?";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(266, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(274, 89);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Какой ты виндросель";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioproverka1);
+            this.groupBox1.Controls.Add(this.radioproverka3);
+            this.groupBox1.Controls.Add(this.radioproverka2);
+            this.groupBox1.Location = new System.Drawing.Point(64, 145);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 171);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(320, 145);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 171);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.uveren3);
+            this.groupBox3.Controls.Add(this.uveren2);
+            this.groupBox3.Controls.Add(this.uveren1);
+            this.groupBox3.Location = new System.Drawing.Point(554, 130);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 173);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::subota_pz_.Properties.Resources._33;
+            this.pictureBox2.Location = new System.Drawing.Point(573, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(155, 133);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::subota_pz_.Properties.Resources._23;
+            this.pictureBox1.Location = new System.Drawing.Point(-7, -77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(981, 641);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.vopros3);
+            this.Controls.Add(this.vopros2);
+            this.Controls.Add(this.vopros1);
+            this.Controls.Add(this.rB_3_3);
+            this.Controls.Add(this.rB_2_2);
+            this.Controls.Add(this.rB_1_1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioproverka1;
+        private System.Windows.Forms.Label vopros1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioproverka2;
+        private System.Windows.Forms.RadioButton radioproverka3;
+        private System.Windows.Forms.RadioButton rB_1_1;
+        private System.Windows.Forms.RadioButton rB_2_2;
+        private System.Windows.Forms.RadioButton rB_3_3;
+        private System.Windows.Forms.RadioButton uveren1;
+        private System.Windows.Forms.RadioButton uveren2;
+        private System.Windows.Forms.RadioButton uveren3;
+        private System.Windows.Forms.Label vopros2;
+        private System.Windows.Forms.Label vopros3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ subota(pz)/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace subota_pz_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioproverka1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isvopros1 =  radioproverka1.Checked;
+            bool isvopros2 = rB_1_1.Checked && rB_2_2.Checked && !rB_3_3.Checked;
+            bool isvopros3 = uveren2.Checked;
+
+            string result = "Result: \n";
+
+            //1 
+            result += isvopros1
+                ? "Q1: corect\n"
+                : "Q1: Uncorect\n";
+            
+            result += isvopros2
+                ? "Q2: corect\n"
+                : "Q2: uncorert\n";
+            
+            result += isvopros3
+                ? "Q3: corect\n"
+                : "Q3: uncorert\n";
+
+            MessageBox.Show(result, "Result");
+
+        }
+
+       
+    }
+}
